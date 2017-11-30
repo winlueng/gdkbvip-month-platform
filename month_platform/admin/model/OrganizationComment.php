4e10b9dbc8a91bc0b17be71b1f0e3006efe47370
winleung <393857054@qq.com>
2017-11-30 18:20:30 +0800

    update sql to org-commit

--- month_platform/admin/model/OrganizationComment.php
+++ month_platform/admin/model/OrganizationComment.php
@@ -3,6 +3,7 @@ namespace app\admin\model;
 
 use think\Model;
 use winleung\exception\WinException;
+use think\Db;
 
 class OrganizationComment extends Common
 {
@@ -15,13 +16,15 @@ class OrganizationComment extends Common
 	public function comment_list()
 	{
 		try {
-			$list = self::where('organization_id', input('get.id'))
+			/*$list = self::where('organization_id', input('get.id'))
 						->where('status', '<>', '-1')
 						->with('User')
 						->order('create_time desc')
 						->paginate(10)
 						->hidden(['update_time'])
-						->toArray();
+						->toArray();*/
+			$oid = input('get.id');
+			// $sql  = "SELECT c.*,u.id,u.nick_name,u.head_url FROM month_organization_service_comment c LEFT JOIN month_organization_service s ON c.service_id=s.id AND s.status<>'-1' LEFT JOIN month_organization o ON o.id=s.organization_id LEFT JOIN month_user u ON u.id=c.user_idWHERE c.status<>'-1' AND o.id={$oid}";
 
 			if (!$list) win_exception('', __LINE__);
 
