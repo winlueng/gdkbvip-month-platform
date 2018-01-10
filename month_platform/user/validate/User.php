9a3fe2c8be010958019d7aaf3ea76e47361db850
winleung <393857054@qq.com>
2018-01-10 18:33:23 +0800

    update user bug

--- month_platform/user/validate/User.php
+++ month_platform/user/validate/User.php
@@ -10,7 +10,7 @@ class User extends Validate
 		['nick_name','require','请输入昵称'],
 		['head_url','require','请输入头像路径'],
 		['sex','require|number|in:0,1,2','请输入性别|性别必需是整数|性别范围只在0,1,2'],
-		['real_name','chsAlpha','性别必需是整数'],
+//		['real_name','chsAlpha','性别必需是整数'],
 		['phone', 'require|length:11', '请输入手机号码|请输入11位有效手机号'],
 		'phone' => ['regex' => '^((13[0-9])|(14[57])|(15[0-6])|(17[1567])|(18[1-9]))\d{8}|(170[4,7-9])\d{7}$'],
 		['password','require','请输入密码'],
